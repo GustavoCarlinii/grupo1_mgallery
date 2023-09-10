6a43bcfa6ab8e965b2ebe707901b0c9aa115214a
divoragu <divoragu12@gmail.com>
2023-09-10 20:20:46 -0300

    servidor activo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = 3030;
+const port = 3040;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -8,18 +8,23 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
+
 app.get('/detalleProducto', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/detalleProducto.html'));
 });
+
 app.get('/carritoCompras', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/carritoCompras.html'));
 });
+
 app.get('/formRegistro', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/formRegistro.html'));
 });
+
 app.get('/formIngreso', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/formIngreso.html'));
 });
+
 app.listen(port, () =>{
     console.log(`Servidor activo en http://localhost:${port}`);
 });
