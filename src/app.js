733729bd5d3a4db265e7809ed5451ac99a859d53
DivoRagu <103603420+divoragu@users.noreply.github.com>
2023-09-10 23:05:50 +0000

    prubeas1

--- src/app.js
+++ src/app.js
@@ -8,7 +8,18 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
-
+app.get('/detalleProducto', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/detalleProducto.html'));
+});
+app.get('/carritoCompras', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/carritoCompras.html'));
+});
+app.get('/formRegistro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/formRegistro.html'));
+});
+app.get('/formIngreso', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/formIngreso.html'));
+});
 app.listen(port, () =>{
     console.log(`Servidor activo en http://localhost:${port}`);
 });
